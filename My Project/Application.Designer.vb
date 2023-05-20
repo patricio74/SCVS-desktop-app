fa9737ad984cf03918e3117f41efda2cf1be5903
jpprz7 <perezjohnpatrick077@gmail.com>
2023-05-21 04:30:26 +0800

    -

--- My Project/Application.Designer.vb
+++ My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.VotingSystem_Perez.scvsAdmin
+            Me.MainForm = Global.VotingSystem_Perez.scvsLogin
         End Sub
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
